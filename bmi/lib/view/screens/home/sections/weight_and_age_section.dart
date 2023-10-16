6536cc7103057a85f58ea6876fbb1e4f7c0f03a0
manarahmedk <manarahmedk@gmail.com>
2023-10-16 03:23:07 +0300

    update with app image

--- bmi/lib/view/screens/home/sections/weight_and_age_section.dart
+++ bmi/lib/view/screens/home/sections/weight_and_age_section.dart
@@ -1,6 +1,7 @@
 import 'package:bmi/view/components/details_widget/details_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import '../../../../view_model/bloc/bmi_cubit/bmi_cubit.dart';
 import '../../../../view_model/bloc/bmi_cubit/bmi_states.dart';
 
@@ -9,7 +10,19 @@ class WeightAndAgetSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<BMICubit,BMIStates>(
-      listener: (context, state) {},
+      listener: (context, state) {
+        if (state is BMIAgeOverState){
+          Fluttertoast.showToast(
+              msg: "This is Center Short Toast",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.CENTER,
+              timeInSecForIosWeb: 1,
+              backgroundColor: Colors.red,
+              textColor: Colors.white,
+              fontSize: 16.0
+          );
+        }
+      },
       builder: (context, state) {
         BMICubit cubit =BMICubit.get(context);
         return Row(
